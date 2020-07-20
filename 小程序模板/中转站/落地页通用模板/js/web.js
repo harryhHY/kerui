c980ff30582ec4768d1298cd4fdbc189b80c2cfb
harry1050732955 <harry1050732955@gmail.com>
2020-07-20 18:53:03 +0800

    落地页通用模板

---
+++ 小程序模板/中转站/落地页通用模板/js/web.js
@@ -0,0 +1,13 @@
+(function (n, e) {
+    var t = n.documentElement,
+        i = "orientationchange" in window ? "orientationchange" : "resize",
+        d = function () {
+            var n = t.clientWidth;
+            n && (t.style.fontSize = n / 7.5 + "px")
+        };
+    n.addEventListener && (e.addEventListener(i, d, !1), n.addEventListener("DOMContentLoaded", d, !1))
+})(document, window);
+var is_mobile = navigator.userAgent.toLowerCase().match(/(ipod|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;   
+if(!is_mobile){
+    window.location.href = "./index.html";
+}
