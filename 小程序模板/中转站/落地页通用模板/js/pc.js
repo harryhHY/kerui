ed72666018ec91d5c2b55b4d883e8f8d19f7b3c5
harry1050732955 <harry1050732955@gmail.com>
2020-07-21 14:13:32 +0800

    图片模板9底库

--- 小程序模板/中转站/落地页通用模板/js/pc.js
+++ 小程序模板/中转站/落地页通用模板/js/pc.js
@@ -1,14 +1,21 @@
-(function (n, e) {
-    var t = n.documentElement,
-        i = "orientationchange" in window ? "orientationchange" : "resize",
-        d = function () {
-            var n = t.clientWidth;
-            n && (t.style.fontSize = n / 9.6 + "px")
-        };
-    n.addEventListener && (e.addEventListener(i, d, !1), n.addEventListener("DOMContentLoaded", d, !1))
-})(document, window);
 var is_mobile = navigator.userAgent.toLowerCase().match(
     /(ipod|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
-if (is_mobile) {
-    window.location.href = "./web.html";
+let url = sessionStorage.getItem('url')
+
+function goto(to) {
+    window.location.href = `./${to}.html`;
+    sessionStorage.setItem('url', to)
+}
+if (url == null) {
+    if (!is_mobile) {
+        goto("index")
+    } else if (is_mobile && url == "web") {
+        goto("web")
+    }
+} else {
+    if (!is_mobile && url == "web") {
+        goto("index")
+    } else if (is_mobile && url == "index") {
+        goto("web")
+    }
 }
