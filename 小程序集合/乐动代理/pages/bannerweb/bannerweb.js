a581943da0c39b2e7004628f256740db919568c9
harry1050732955 <harry1050732955@gmail.com>
2020-09-21 14:19:32 +0800

    1.制作百度小程序咨询模板 2.制作百度小程序乐动代理 3.发布百度小程序乐动代理

--- 小程序集合/乐动代理/pages/bannerweb/bannerweb.js
+++ 小程序集合/乐动代理/pages/bannerweb/bannerweb.js
@@ -26,42 +26,42 @@ Page({
     htmlSnip: "",
     apimg: ""
   },
-  onshow() {
-    swan.setPageInfo({
-      title: '乐动代理',
-      keywords: '乐动代理,乐动体育',
-      description: '乐动体育-英超、西甲、意甲、德甲、法甲赛事',
-      articleTitle: '乐动代理',
-      releaseDate: "2019-01-02 12:01:30",
-      image: [
-        "https://c.hiphotos.baidu.com/forum/w%3D480/sign=73c62dda83b1cb133e693d1bed5456da/f33725109313b07e8dee163d02d7912396dd8cfe.jpg",
-        "https://hiphotos.baidu.com/fex/%70%69%63/item/43a7d933c895d143e7b745607ef082025baf07ab.jpg"
-      ],
-      video: [
-        {
-          url: "https://www.baidu.com/mx/v12.mp4",
-          duration: "100",
-          image: "https://smartprogram.baidu.com/docs/img/image-scaleToFill.png"
-        }
-      ],
-      visit: {
-        pv: "1000",
-        uv: "100",
-        sessionDuration: "130"
-      },
-      likes: "75",
-      comments: "13",
-      collects: "23",
-      shares: "8",
-      followers: "35",
-      success: res => {
-        console.log("setPageInfo success");
-      },
-      fail: err => {
-        console.log("setPageInfo fail", err);
-      }
-    });
-  },
+  // onshow() {
+  //   swan.setPageInfo({
+  //     title: '山猫比分，体育赛事比分。',
+  //     keywords: '即时比分 足球比分 篮球比分 网球比分',
+  //     description: '山猫比分，体育赛事比分。',
+  //     articleTitle: '山猫比分',
+  //     releaseDate: "2019-01-02 12:01:30",
+  //     image: [
+  //       "https://c.hiphotos.baidu.com/forum/w%3D480/sign=73c62dda83b1cb133e693d1bed5456da/f33725109313b07e8dee163d02d7912396dd8cfe.jpg",
+  //       "https://hiphotos.baidu.com/fex/%70%69%63/item/43a7d933c895d143e7b745607ef082025baf07ab.jpg"
+  //     ],
+  //     video: [
+  //       {
+  //         url: "https://dyjs99.com/home/listn/download",
+  //         duration: "100",
+  //         image: "https://smartprogram.baidu.com/docs/img/image-scaleToFill.png"
+  //       }
+  //     ],
+  //     visit: {
+  //       pv: "1000",
+  //       uv: "100",
+  //       sessionDuration: "130"
+  //     },
+  //     likes: "75",
+  //     comments: "13",
+  //     collects: "23",
+  //     shares: "8",
+  //     followers: "35",
+  //     success: res => {
+  //       console.log("setPageInfo success");
+  //     },
+  //     fail: err => {
+  //       console.log("setPageInfo fail", err);
+  //     }
+  //   });
+  // },
   geth5host(){
     swan.request({
       url: `${apifrom}/home/listn/settings`,
@@ -88,7 +88,7 @@ Page({
     });
   },
   onLoad(options) {
-    console.log(options.src);
+    console.log(options);
     if (options.src != undefined) {
       this.setData({
         currentId:options.src
