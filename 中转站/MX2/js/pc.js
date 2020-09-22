7e561bba02bc6f5a799a0ac12f342cb6b0fb18cf
harry1050732955 <harry1050732955@gmail.com>
2020-09-22 21:07:25 +0800

    ManBetX落地页面

--- 中转站/MX2/js/pc.js
+++ 中转站/MX2/js/pc.js
@@ -1,21 +1,24 @@
-var is_mobile = navigator.userAgent.toLowerCase().match(
-    /(ipod|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
-let url = sessionStorage.getItem('url')
-
 function goto(to) {
     window.location.href = `./${to}.html`;
     sessionStorage.setItem('url', to)
 }
-if (url == null) {
-    if (!is_mobile) {
-        goto("index")
-    } else if (is_mobile && url == "web") {
-        goto("web")
-    }
-} else {
-    if (!is_mobile && url == "web") {
-        goto("index")
-    } else if (is_mobile && url == "index") {
-        goto("web")
+
+function pipixia() {
+    var is_mobile = navigator.userAgent.toLowerCase().match(
+        /(ipod|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
+    var url = sessionStorage.getItem('url')
+    if (url == null) {
+        if (!is_mobile) {
+            goto("index")
+        } else if (is_mobile) {
+            goto("web")
+        }
+    } else {
+        if (!is_mobile && url == "web") {
+            goto("index")
+        } else if (is_mobile && url == "index") {
+            goto("web")
+        }
     }
 }
+pipixia()
