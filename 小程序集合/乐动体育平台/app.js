cd611ad50e4919bea27b7e704d58fb215559cc82
harry1050732955 <harry1050732955@gmail.com>
2020-09-22 14:36:25 +0800

    MX2 咨询模板制作发布

---
+++ 小程序集合/乐动体育平台/app.js
@@ -0,0 +1,33 @@
+/**
+ * @file app.js
+ * @author swan
+ */
+
+/* globals swan */
+App({
+    api: "https://z5mh2.cn",
+    apifrom: "https://z5mh2.cn",
+    c:205,
+    onLaunch(options) {
+        // do something when launch
+        // 引导添加，参见文档： http://smartprogram.baidu.com/docs/design/component/guide_add/
+        if (swan.canIUse('showFavoriteGuide')) {
+            swan.showFavoriteGuide({
+                type: 'bar',
+                content: '一键关注小程序',
+                success(res) {
+                    console.log('关注成功：', res);
+                },
+                fail(err) {
+                    console.log('关注失败：', err);
+                }
+            });
+        }
+    },
+    onShow(options) {
+        // do something when show
+    },
+    onHide() {
+        // do something when hide
+    }
+});
