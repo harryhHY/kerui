3417d4c5ef01a025ad0429e9afe5030e66499220
harry1050732955 <harry1050732955@gmail.com>
2020-10-12 11:55:11 +0800

    比分模板2.0 7底库

---
+++ 小程序模板/比分模板2.0/imgmobanrace7/app.js
@@ -0,0 +1,39 @@
+/**
+ * @file app.js
+ * @author swan
+ */
+
+/* globals swan */
+App({
+  apifrom: "https://z5mh2.cn",
+  api: "https://f9f7t.cn",
+  c:113,
+  little: {
+    title: '百事通直播,精彩赛事直播。',
+    keywords: '百事通直播,篮球直播,足球直播',
+    description: '百事通直播,精彩赛事直播。',
+    articleTitle: '百事通直播',
+},
+  onLaunch(options) {
+    // do something when launch
+    // 引导添加，参见文档： http://smartprogram.baidu.com/docs/design/component/guide_add/
+    if (swan.canIUse("showFavoriteGuide")) {
+      swan.showFavoriteGuide({
+        type: "bar",
+        content: "一键关注小程序",
+        success(res) {
+          console.log("关注成功：", res);
+        },
+        fail(err) {
+          console.log("关注失败：", err);
+        }
+      });
+    }
+  },
+  onShow(options) {
+    // do something when show
+  },
+  onHide() {
+    // do something when hide
+  }
+});
